98848f0a6ab86a760b0a67f02847f7720f1f8760
Oli <oli@olillin.com>
2024-09-29 02:40:29 +0200

    Fix silly port bug
    Don't bitwise OR your ports

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express()
 
 app.use('/', express.static('./public'))
 
-const port = process.env.PORT | 8080
+const port = process.env.PORT ?? 8080
 app.listen(port)
 console.log(`Listening on port ${port}`)
